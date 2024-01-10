3e4807db9473102f8a46ee18451f3b73cf465613
Shri Srivastava <76124911+shri33@users.noreply.github.com>
2024-01-11 04:02:49 +0530

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,7 @@
 // Login.jsx
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
+//import { signInWithEmailAndPassword } from "firebase/auth";
 
 import InputControl from "../InputControl/InputControl";
 //import { auth } from "../../firebase";
